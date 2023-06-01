d5ee6af192fd85fdbe6a62ab0c40e7fdc9b6cb03
okazu <kazuya_y_y_y@outlook.jp>
2023-06-01 22:15:11 +0900

    fdfdfd

--- setting.py
+++ setting.py
@@ -2,7 +2,7 @@ import requests
 headers = {"Content-Type": "application/vnd.github+json"}
 
 payload = {
-    "event_type": "my-custom-event",
+    "event_type": "i",
     "client_payload":{
         "Accept": "application/vnd.github+json",
         "Authorization": "Bearer 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",
