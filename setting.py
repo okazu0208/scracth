036517eedb557b5aa50b708326ac4f86c9f9bca6
okazu <kazuya_y_y_y@outlook.jp>
2023-06-01 22:19:01 +0900

    ewewe

--- setting.py
+++ setting.py
@@ -5,5 +5,5 @@ payload = {
     "event_type": "i",
     "client_payload":{
         "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer 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",
         "X-GitHub-Api-Version": "2022-11-28",}}
