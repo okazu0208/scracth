2c9d3f8e8a77628304ed4f6bd4f5d145f34cb85e
okazu <kazuya_y_y_y@outlook.jp>
2023-06-01 22:21:10 +0900

    rerere

--- setting.py
+++ setting.py
@@ -5,5 +5,5 @@ payload = {
     "event_type": "i",
     "client_payload":{
         "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer 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",
         "X-GitHub-Api-Version": "2022-11-28",}}
