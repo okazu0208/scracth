edc8d38deadc3fd65b426d160eb3ebbd6c2c7f94
okazu <kazuya_y_y_y@outlook.jp>
2023-06-01 21:48:28 +0900

    efefefe

--- setting.py
+++ setting.py
@@ -4,5 +4,6 @@ headers = {"Content-Type": "application/vnd.github+json"}
 payload = {
     "event_type": "my-custom-event",
     "client_payload":{
-        "key1": "value1",
-        "key2": "value2"}}
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer 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",
+        "X-GitHub-Api-Version": "2022-11-28",}}
