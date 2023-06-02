09fee6e76d1379ceb0cfcee3b3e3b98a563acd46
okazu <kazuya_y_y_y@outlook.jp>
2023-06-02 12:43:02 +0900

    pou

--- scratch.py
+++ scratch.py
@@ -4,6 +4,10 @@ os.system("pip install -U scratchattach")
 import scratchattach as scratch3
 session = scratch3.Session(".eJxVT0tLxDAQ_i89a22TtCZ7WwVdEMQHHtxLmU2mbbZtUpqUguJ_dwJ72cPAzPfim99sDbg4mDDbZUEvEHX_tvimYNlN1sAa-yYJGmuIV0pwVtYlURFD1N4PNvk2vwxorg0n0AO65EoYumg1ROtdfiFC_oHzeAEfLmLK9bSQqVIMoJWmrYELlCVIbgrVSjBSSMPa3bZnffH1uIUXyZ6Oh9kfDQrx-X54DhQz-s66WzunJJnfFzmrc1mnhiO4boUu1T4DAeZMgG-infDHuwTvJ1yo190rbs03fXb9Vw-hJ5EApamiAcNrIzQXhRI0FTcay7biqICuos7-_gGaq3JW:1ppowi:RzXQOXVMhIb_NLKqxa4gUZDeLfk", username="scratchPro_02")
 conn = scratch3.CloudConnection(project_id = "838970827", username="scratchPro_02", session_id=".eJxVT0tLxDAQ_i89a22TtCZ7WwVdEMQHHtxLmU2mbbZtUpqUguJ_dwJ72cPAzPfim99sDbg4mDDbZUEvEHX_tvimYNlN1sAa-yYJGmuIV0pwVtYlURFD1N4PNvk2vwxorg0n0AO65EoYumg1ROtdfiFC_oHzeAEfLmLK9bSQqVIMoJWmrYELlCVIbgrVSjBSSMPa3bZnffH1uIUXyZ6Oh9kfDQrx-X54DhQz-s66WzunJJnfFzmrc1mnhiO4boUu1T4DAeZMgG-infDHuwTvJ1yo190rbs03fXb9Vw-hJ5EApamiAcNrIzQXhRI0FTcay7biqICuos7-_gGaq3JW:1ppowi:RzXQOXVMhIb_NLKqxa4gUZDeLfk")
-#main
-i = scratch3.get_var("838970827","e")
-print (i)
+r = 10
+while r == 0:
+ #main
+ i = scratch3.get_var("838970827","e")
+ print (i)
+ #数える
+ r - 1
